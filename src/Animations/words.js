c01ff9d8ba7eefbef9a376fc652a47ac880a655a
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-11-20 13:36:05 +0530

    Add files via upload

---
+++ src/Animations/words.js
@@ -0,0 +1,17 @@
+import { TIME } from './Words/TIME';
+import { HOME } from './Words/HOME';
+import { PERSON } from './Words/PERSON';
+import { YOU } from './Words/YOU';
+import {NO} from './Words/NO';
+import {THANK} from './Words/THANK';
+import {ABOVE} from './Words/ABOVE';
+import {WE} from './Words/WE';
+import {CLAPS} from './Words/CLAPS'
+import {GOOD} from './Words/GOOD'
+
+
+var wordList = ['TIME', 'HOME', 'PERSON', 'YOU','NO','THANK','ABOVE', 'WE','CLAPS','GOOD'];
+
+export {
+    TIME, HOME, PERSON, YOU ,NO,THANK,ABOVE, WE,CLAPS,GOOD, wordList
+}
